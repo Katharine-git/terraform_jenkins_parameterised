271222d62e6050b3bba6a3ea2bb08a26937f8eb4
Katharine-git <78786279+Katharine-git@users.noreply.github.com>
2021-07-30 12:33:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" { default = "us-east-1" }
-variable "vpc_cidr_block" { default = "${var.vpc_cidr_block}" }
-variable "aws_az" {default = "${var.aws_az}"}
-variable "vpc_tag" { default = "${var.vpc_tag}"}
-variable "subnet_tag" { default = "${var.subnet_name}"}
+variable "vpc_cidr_block" { default = "172.20.0.0/16}
+variable "aws_az" {default = "us-east-1a" ]
+variable "vpc_tag" { default = "ust_vpc" }
+variable "subnet_tag" { default = "ust_subnet"}
